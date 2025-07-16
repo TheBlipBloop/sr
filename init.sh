51a00c7edf3eba23a6ac0390be1f7c3c4a44a83f
TheBlipBloop <blipbloopdev@gmail.com>
2025-07-15 23:12:30 -0700

    typo --- validated and init script gets everything up and running!

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # sync submodules 
-git submodules update --init --recursive 
+git submodule update --init --recursive 
 
 # pull in the shaderc utils 
 ./external/shaderc/utils/git-sync-deps
